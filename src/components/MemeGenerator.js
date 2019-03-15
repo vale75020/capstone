13a1c32bef08e0a7ff96eb932e7eba26a1cad1b9
vale75020 <valentinosuma@gmail.com>
2019-03-15 14:53:13 +0100

    submit

--- src/components/MemeGenerator.js
+++ src/components/MemeGenerator.js
@@ -16,27 +16,49 @@ export default class MemeGenerator extends Component {
       .then(response => response.json())
       .then(response => {
         const { memes } = response.data;
-        console.log(memes[0]);
+    //  console.log(memes[0]);
         this.setState({ allMemeImgs: memes });
       });
   }
 
-  handleChange = (event) => {
-   // console.log('it works!')
-   const { name, value } = event.target
-   this.setState({ [name] : value })
+  handleChange = event => {
+    // console.log('it works!')
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    //  get a random number int (index in array)
+    const randNum = Math.floor(Math.random()*this.state.allMemeImgs.length)
+    //  get the meme from that index
+    const randMemeImg = this.state.allMemeImgs[randNum].url
+    //  set "rendomImg to the 'url" of the random item
+    this.setState({ randomImg: randMemeImg })
   }
 
   render() {
     return (
       <div>
-        <form>
-          <input type="text" placeholder="Top Text" name="topText" value={this.state.topText} onChange={this.handleChange} />
-          <input type="text" placeholder="Bottom Text" name="bottomText" value={this.state.bottomText} onChange={this.handleChange} />
+        <form className="meme-form" onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Top Text"
+            name="topText"
+            value={this.state.topText}
+            onChange={this.handleChange}
+          />
+          <input
+            type="text"
+            placeholder="Bottom Text"
+            name="bottomText"
+            value={this.state.bottomText}
+            onChange={this.handleChange}
+          />
           <button>GEN</button>
         </form>
         <div className="meme">
-          <img src={this.state.randomImg} alt="new meme"/>
+          <img src={this.state.randomImg} alt="new meme" />
           <h2 className="top">{this.state.topText}</h2>
           <h2 className="bottom">{this.state.bottomText}</h2>
         </div>
